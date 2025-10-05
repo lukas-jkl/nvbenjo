33e24dad367596c7b289344805365669612a8708
lukas_jkl <60255100+lukas-jkl@users.noreply.github.com>
2025-10-05 20:51:22 +0200

    Add current working dir to search path for config files

--- nvbenjo/plugin.py
+++ nvbenjo/plugin.py
@@ -5,3 +5,5 @@ from hydra.plugins.search_path_plugin import SearchPathPlugin
 class NvbenjoSearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         search_path.append(provider="nvbenjo-plugin", path="pkg://nvbenjo/conf")
+        # allow from current directory
+        search_path.append(provider="nvbenjo-user", path="file://${hydra.runtime.cwd}")
