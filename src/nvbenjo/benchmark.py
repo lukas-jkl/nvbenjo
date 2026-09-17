54328a2be4373915974e40edec2332690067c964
lukas_jkl <60255100+lukas-jkl@users.noreply.github.com>
2026-09-17 18:42:23 +0200

    Drop numpy import only used for sentinal

--- src/nvbenjo/benchmark.py
+++ src/nvbenjo/benchmark.py
@@ -1,12 +1,12 @@
 import functools
 import itertools
 import logging
+import math
 import time
 import typing as ty
 from collections.abc import Callable
 from typing import Any
 
-import numpy as np
 import pandas as pd
 import torch
 from rich import progress
@@ -321,7 +321,7 @@ def benchmark_model(
     )
     bench_task = progress_bar.add_task("Running Benchmark", total=len(iter_cfgs))
     for device_str, batch_size, (runtime_option_name, runtime_cfg) in iter_cfgs:
-        if precision_batch_oom.get(runtime_option_name, np.inf) < batch_size:
+        if precision_batch_oom.get(runtime_option_name, math.inf) < batch_size:
             # already went oom for these runtime options with smaller batch size -> skip bigger one
             progress_bar.advance(bench_task)
             continue
